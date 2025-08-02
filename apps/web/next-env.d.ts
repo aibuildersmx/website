9c62167129299345aa9ec3cd28482be4a2e7fcb9
Locke <benkim96@gmail.com>
2025-08-02 16:13:58 -0600

    .env

---
+++ apps/web/next-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    NEXT_PUBLIC_CLERK_SECRET_KEY: string;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_OUT_URL: string;
+  }
+}
